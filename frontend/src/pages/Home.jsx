2abac5d3bb724a77e7398ec54347b6a2bdb5d944
Sree Navasree <ksnavasree@gmail.com>
2026-07-28 19:36:14 +0530

    JavaScript XML created

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+function Home() {
+
+    return (
+
+        <div style={{textAlign:"center",marginTop:"100px"}}>
+
+            <h1>LegalConnect</h1>
+
+            <h3>Web-Based Legal Assistance Portal</h3>
+
+            <p>Welcome to LegalConnect</p>
+
+        </div>
+
+    );
+
+}
+
+export default Home;
